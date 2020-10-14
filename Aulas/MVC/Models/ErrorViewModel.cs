0944eb5d966eb3587645675f01c4b975cb42e39c
OnikenX <onikenx@protonmail.com>
2020-10-14 17:35:24 +0100

    created the root folder

---
+++ Aulas/MVC/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Aulas.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
